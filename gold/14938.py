e8f58511beab47bca4029ec41bcc379852ae9699
cih831 <choi83195@gmail.com>
2023-02-16 06:30:22 +0900

    0216

--- gold/14938.py
+++ gold/14938.py
@@ -1,20 +1,29 @@
 import sys
+import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
 N, M, R = map(int, input().split())
-item = list(map(int, input().split()))
-graph = [[] for _ in range(N)]
-dist = [[INF * N] for _ in range(N)]
+item = [0] + list(map(int, input().split()))
+dist = [[INF] * (N + 1) for _ in range(N + 1)]
 
-for _ in range(N):
+for _ in range(R):
     s, e, t = map(int, input().split())
-    graph[s].append([e, t])
-    graph[e].append([s, t])
     dist[s][e] = t
     dist[e][s] = t
 
-for i in range(N):
-    for j in range(N):
-        if i != j:
-            pass
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+
+ans = 0
+for i in range(1, N + 1):
+    tmp = 0
+    for j in range(1, N + 1):
+        if dist[i][j] <= M or i == j:
+            tmp += item[j]
+    ans = max(ans, tmp)
+
+print(ans)
